d4d11ea9f790965d77f8ecf175c3c068d6762213
yangganggood <yanggangc@163.com>
2018-10-31 00:12:10 +0800

    add list

---
+++ list/list.go
@@ -0,0 +1,60 @@
+package list
+
+import (
+	"fmt"
+)
+
+// Node 单链表结构
+type Node struct {
+	Data interface{}
+	Next *Node
+}
+
+// List 链表结构
+type List struct {
+	size uint64
+	head *Node // 头
+	tail *Node // 尾
+}
+
+// Print 打印节点元素
+//
+// head 指向链表头节点的元素
+func Print(head *Node) {
+	p := head
+	if p == nil {
+		fmt.Printf("node is empty ... \n")
+		return
+	}
+
+	for p != nil {
+		fmt.Printf("%v ... \n", p.Data)
+
+		p = p.Next
+	}
+}
+
+// Reverse 链表反转
+//
+// head 链表的起始节点
+func Reverse(head *Node) {
+	if head == nil {
+		fmt.Printf("node is empty ... \n")
+		return
+	}
+
+	p := head // p 用于记录当前节点
+	next := head.Next
+
+	p.Next = nil // 将 head 节点的 next 指向 nil
+
+	// 从第二个节点开始
+	for next != nil {
+		pNext := next.Next // 临时获取下一个节点
+
+		next.Next = p
+		p = next
+
+		next = pNext
+	}
+}
---
+++ list/list_test.go
@@ -0,0 +1,22 @@
+package list
+
+import "testing"
+
+func TestReverse(t *testing.T) {
+	node := &Node{
+		Data: 1,
+		Next: &Node{
+			Data: 2,
+			Next: &Node{
+				Data: 3,
+				Next: nil,
+			},
+		},
+	}
+
+	Reverse(node)
+
+	Print(node)
+
+	t.Fail()
+}
