1822c52965f193c405dbfb28e84d0c3a44e17129
Gang Yang <gang.yang.o@nio.com>
2018-11-09 15:22:11 +0800

    ready shell

--- sort/shell.go
+++ sort/shell.go
@@ -1,6 +1,6 @@
 package sort
 
-// Shell 希尔排序
+// Shell 希尔排序（分组插入排序）
 type Shell struct {
 }
 
@@ -11,6 +11,18 @@ type Shell struct {
 // 优点：
 // 缺点
 func (s *Shell) Sort(arr []int) []int {
+	if arr == nil || len(arr) == 0 || len(arr) == 1 {
+		return arr
+	}
+
+	gap := len(arr) / 2 // 元素间隔，初始值为数组的一半; 随便也给定初始化的分组数目
+	for gap > 0 {
+		for i := 0; i < gap; i++ {
+
+		}
+
+		gap = gap >> 1 // (gap / 2)
+	}
 
 	return nil
 }
