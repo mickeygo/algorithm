9b3c0a14c7566a5c3365fb78c14019e99395bcba
yangganggood <yanggangc@163.com>
2019-11-01 23:08:00 +0800

    add graph

---
+++ graph/graph.go
@@ -0,0 +1,26 @@
+package graph
+
+type Graph struct {
+
+}
+
+// V 定点数
+func (g *Graph) V() int {
+	return 0
+}
+
+// E 边数
+func (g *Graph) E() int {
+	return 0
+}
+
+// AddEage 向图中添加一条边 v-w
+func (g *Graph) AddEage(v, w int) {
+
+}
+
+// Adj 和 v 相邻的所有顶点
+func (g *Graph) Adj(v int) []int {
+	return nil
+}
+
